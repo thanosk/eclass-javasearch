957225527016a5229517534638fc33e860450c29
Thanos Kyritsis <djart@blackmajesty>
2012-11-08 18:17:31 +0200

    update mysql connector version numbers in scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 javac -d bin \
--cp lib/mysql-connector-java-5.1.18-bin.jar \
+-cp lib/mysql-connector-java-5.1.22-bin.jar \
 -cp lib/lucene-core-3.6.1.jar \
 ./src/java/org/openeclass/lucene/demo/IndexCourses.java \
 ./src/java/org/openeclass/lucene/demo/PropertyLoader.java
--- indexer.sh
+++ indexer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-java -cp bin:lib/lucene-core-3.6.1.jar:lib/mysql-connector-java-5.1.18-bin.jar \
+java -cp bin:lib/lucene-core-3.6.1.jar:lib/mysql-connector-java-5.1.22-bin.jar \
 org.openeclass.lucene.demo.IndexCourses $1
